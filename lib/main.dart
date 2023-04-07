52ae67f48110d3b752ad8efaec8590a8cbbff327
GolubevMaxim <vfrc008@icloud.com>
2023-04-07 03:08:09 +0300

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,159 @@
+// import 'package:flutter/material.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:music_statistic_application/widgets/app.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Hive.initFlutter();
+//   runApp(const App());
+// }
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class Example extends StatelessWidget {
+  const Example({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return const Scaffold(
+      body: SafeArea(
+        child: SimpleCalcWidget(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SimpleCalcWidget extends StatefulWidget {
+  const SimpleCalcWidget({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SimpleCalcWidgetState createState() => _SimpleCalcWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _SimpleCalcWidgetState extends State<SimpleCalcWidget> {
+  final _model = SimpleCalcWidgetModel();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: ChangeNotifierProvider(
+          model: _model,
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FirstNumberWidget(),
+              SizedBox(height: 10),
+              SecondNumberWidget(),
+              SizedBox(height: 10),
+              SummButtonWidget(),
+              SizedBox(height: 10),
+              ResultWidget(),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class FirstNumberWidget extends StatelessWidget {
+  const FirstNumberWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(border: OutlineInputBorder()),
+      onChanged: (String value) =>
+          ChangeNotifierProvider.read<SimpleCalcWidgetModel>(context)
+              ?.firstNumber = value,
+    );
+  }
+}
+
+class SecondNumberWidget extends StatelessWidget {
+  const SecondNumberWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(border: OutlineInputBorder()),
+      onChanged: (String value) =>
+          ChangeNotifierProvider.read<SimpleCalcWidgetModel>(context)
+              ?.secondNumber = value,
+    );
+  }
+}
+
+class SummButtonWidget extends StatelessWidget {
+  const SummButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () =>
+          ChangeNotifierProvider.read<SimpleCalcWidgetModel>(context)?.summ(),
+      child: const Text('Посчитать сумму'),
+    );
+  }
+}
+
+class ResultWidget extends StatelessWidget {
+  const ResultWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final value = ChangeNotifierProvider.watch<SimpleCalcWidgetModel>(context)
+            ?.summResult ??
+        '-1';
+    return Text('Результат: $value');
+  }
+}
+
+class SimpleCalcWidgetModel extends ChangeNotifier {
+  int? _firstNumber;
+  int? _secondNumber;
+  int? summResult;
+
+  set firstNumber(String value) => _firstNumber = int.tryParse(value);
+  set secondNumber(String value) => _secondNumber = int.tryParse(value);
+
+  void summ() {
+    if (_firstNumber != null && _secondNumber != null) {
+      summResult = _firstNumber! + _secondNumber!;
+    } else {
+      summResult = -1;
+    }
+    notifyListeners();
+  }
+}
+
+class ChangeNotifierProvider<T extends ChangeNotifier>
+    extends InheritedNotifier<T> {
+  const ChangeNotifierProvider({
+    Key? key,
+    required T model,
+    required Widget child,
+  }) : super(
+          key: key,
+          notifier: model,
+          child: child,
+        );
+
+  static T? watch<T extends ChangeNotifier>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<ChangeNotifierProvider<T>>()
+        ?.notifier;
+  }
+
+  static T? read<T extends ChangeNotifier>(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<ChangeNotifierProvider<T>>()
+        ?.widget;
+    if (widget is ChangeNotifierProvider<T>) {
+      return widget.notifier;
+    } else {
+      return null;
+    }
   }
 }
